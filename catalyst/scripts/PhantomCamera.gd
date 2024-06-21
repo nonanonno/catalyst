762f11fced1e0f6a9d6fbb7b3ce3893cc5cb07fe
nonanonno <as.nonanonno@gmail.com>
2024-06-21 14:16:23 +0000

    feat: change phantom camera behavior

--- catalyst/scripts/PhantomCamera.gd
+++ catalyst/scripts/PhantomCamera.gd
@@ -1,50 +1,66 @@
+# Phantom camera script. The camera is controlled like a FPS.
 extends Camera3D
 
 @export_range(0, 10, 0.01) var sensitivity: float = 3
-@export_range(0, 10, 0.01) var speed_scale: float = 1.17
-@export_range(1, 100, 0.1) var boost_speed_multiplier: float = 3.0
+@export_range(1, 100, 1) var acceleration: float = 10
+@export_range(0, 100, 1) var max_speed: float = 50
 
-@onready var _velocity = 3
-@onready var rot_x = 0
-@onready var rot_y = 0
+@onready var window_planar_rot: Vector2 = Vector2.ZERO
+@onready var last_direction: Vector3 = Vector3.ZERO
+@onready var last_elevation: float = 0
+@onready var speed: float = 0
+
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 func _input(event):
 	if not current:
 		return
-	
+
+	# Change camera direction if the mode is captured (aka FPS movement)
 	if Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
 		if event is InputEventMouseMotion:
-			rot_x -= event.relative.x / 1000 * sensitivity
-			rot_y -= event.relative.y / 1000 * sensitivity
-			rot_y = clamp(rot_y, -PI/2, PI/2)
-			transform.basis = Basis()
-			rotate_object_local(Vector3(0,1,0), rot_x)
-			rotate_object_local(Vector3(1,0,0), rot_y)
-			
-	
-	if event is InputEventMouseButton:
-		match event.button_index:
-			MOUSE_BUTTON_RIGHT:
-				Input.mouse_mode = Input.MOUSE_MODE_CAPTURED if event.pressed else Input.MOUSE_MODE_VISIBLE
-			MOUSE_BUTTON_WHEEL_UP:
-				_velocity = clamp(_velocity * speed_scale, 0.2, 1000)
-			MOUSE_BUTTON_WHEEL_DOWN:
-				_velocity = clamp(_velocity / speed_scale, 0.2, 1000)
+			# Limit camera pitch to avoid flipping
+			window_planar_rot.y = clamp(window_planar_rot.y - event.relative.y / 1000 * sensitivity, -PI / 2, PI / 2)
+			window_planar_rot.x = fmod(window_planar_rot.x - event.relative.x / 1000 * sensitivity, 2 * PI)
 
+			transform.basis = Basis()
+			rotate_object_local(Vector3(0, 1, 0), window_planar_rot.x) # first rotate in Y
+			rotate_object_local(Vector3(1, 0, 0), window_planar_rot.y) # then rotate in X
+
+	# Switch capture mode
+	if event is InputEventKey and event.is_action_pressed("Menu"):
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE if Input.mouse_mode == Input.MOUSE_MODE_CAPTURED else Input.MOUSE_MODE_CAPTURED)
 
 func _process(delta):
 	if not current:
 		return
-	
+
+	# Do not move the camera if the mouse is visible mode (i.e. GUI)
+	if Input.mouse_mode == Input.MOUSE_MODE_VISIBLE:
+		return
+
+	# Capture the control order
 	var direction = Vector3(
-		float(Input.is_physical_key_pressed(KEY_D)) - float(Input.is_physical_key_pressed(KEY_A)),
-		0,
-		float(Input.is_physical_key_pressed(KEY_S)) - float(Input.is_physical_key_pressed(KEY_W))
-	).normalized()
-	
-	if Input.is_physical_key_pressed(KEY_SHIFT):
-		translate(direction * _velocity * delta * boost_speed_multiplier)
-		global_translate(Vector3(0,float(Input.is_physical_key_pressed(KEY_SPACE)) - float(Input.is_physical_key_pressed(KEY_CTRL)),0) * _velocity * delta * boost_speed_multiplier)
+		float(Input.is_action_pressed("Right")) - float(Input.is_action_pressed("Left")),
+		float(Input.is_action_pressed("Up")) - float(Input.is_action_pressed("Down")),
+		float(Input.is_action_pressed("Back")) - float(Input.is_action_pressed("Forward"))
+	)
+
+	var boost = float(Input.is_action_pressed("Boost"))
+
+	# Change the speed
+	if direction.length() > 0:
+		speed = min(max_speed * (boost + 1), speed + acceleration * (boost + 2) * delta)
 	else:
-		translate(direction * _velocity * delta)
-		global_translate(Vector3(0,float(Input.is_physical_key_pressed(KEY_SPACE)) - float(Input.is_physical_key_pressed(KEY_CTRL)),0) * _velocity * delta)
+		speed = 0
+
+	if direction.length() > 0:
+		direction = direction.normalized()
+		last_direction = direction
+	else:
+		direction = last_direction
+
+	# Move the camera
+	direction = direction.rotated(Vector3.UP, window_planar_rot.x)
+	global_translate(direction * speed * delta)
